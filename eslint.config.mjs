a6582cd6edb9fcf572b1879258845db7396b4dd1
Truong Nguyen <ndtrg281@gmail.com>
2025-09-06 16:12:40 +0700

    feat: update project name and dependencies to NestJS 11
    - Changed project name from 'beginer_nestjs' to 'crud'.
    - Updated NestJS dependencies to version 11.0.1.
    - Updated reflect-metadata and other dev dependencies to their latest versions.
    - Added PostModule to the AppModule imports.
    - Fixed formatting issues in app.service.ts and app.e2e-spec.ts.
    - Updated TypeScript configuration to use nodenext module resolution and ES2023 target.
    - Introduced PostModule with a basic structure.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,34 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import eslintPluginPrettier from 'eslint-plugin-prettier'
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-export default [
-    { files: ['**/*.{js,mjs,cjs,ts}'] },
-    { languageOptions: { globals: globals.node } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    {
-        plugins: {
-            prettier: eslintPluginPrettier
-        },
-        rules: {
-            '@typescript-eslint/no-explicit-any': 'warn',
-            '@typescript-eslint/no-unused-vars': 'warn',
-            'prettier/prettier': [
-                'warn',
-                {
-                    arrowParens: 'always',
-                    semi: false,
-                    trailingComma: 'none',
-                    tabWidth: 2,
-                    endOfLine: 'auto',
-                    useTabs: false,
-                    singleQuote: true,
-                    printWidth: 120,
-                    jsxSingleQuote: true
-                }
-            ]
-        },
-        ignores: ['**/node_modules/', '**/dist/']
-    }
-]
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn'
+    },
+  },
+);
