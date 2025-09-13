78f2b7b88a39d936fd17d6f8703632a3693c1b52
Truong Nguyen <ndtrg281@gmail.com>
2025-09-13 15:45:06 +0700

    feat: enhance authentication DTOs with password validation and add custom IsMatch decorator

--- src/routes/auth/auth.dto.ts
+++ src/routes/auth/auth.dto.ts
@@ -1,10 +1,12 @@
-import { IsEmail, IsString } from 'class-validator'
+import { IsEmail, IsString, Length } from 'class-validator'
 import { UserEntity } from 'src/routes/auth/entities/user.entity'
 import { OmitType } from '@nestjs/mapped-types'
+import { IsMatch } from 'src/shared/decorators/custom-validator.decorator'
 export class LoginBodyDto {
   @IsEmail()
   email: string
   @IsString()
+  @Length(6, 20, { message: 'Password must be between 6 and 20 characters' })
   password: string
 }
 
@@ -12,6 +14,8 @@ export class RegisterBodyDto extends LoginBodyDto {
   @IsString({ message: 'Name must be a string' })
   name: string
   @IsString()
+  @Length(6, 20, { message: 'Password must be between 6 and 20 characters' })
+  @IsMatch('password', { message: 'Confirm password does not match' })
   confirmPassword: string
 }
 
---
+++ src/shared/decorators/custom-validator.decorator.ts
@@ -0,0 +1,27 @@
+import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator'
+
+export function IsMatch(property: string, validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'isMatch',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [property],
+      options: validationOptions,
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          const [relatedPropertyName] = args.constraints
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+          const relatedValue = (args.object as any)[relatedPropertyName]
+          return value === relatedValue
+        },
+        defaultMessage(args: ValidationArguments) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          const [relatedPropertyName] = args.constraints
+          return `${args.property} must match ${relatedPropertyName}`
+        },
+      },
+    })
+  }
+}
